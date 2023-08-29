1e6dc480cfefd8a78a45caf7c121042c6e0b2cce
EndrexAkoto <martinakoto3@gmail.com>
2023-08-29 04:58:08 -0700

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,8 +2,7 @@
 
 /**
  * binary_tree_size - Measures size of binary tree
- * @tree: Pointer to root node of tree to measure the size 
- *
+ * @tree: Pointer to root node of tree to measure the size
  * Return: Size of tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
